5196ea7d8842beece9c7a02887a19f5786832305
Masuu <mrpacharya@gmail.com>
2022-08-06 14:38:31 +0545

    modified form

---
+++ script.js
@@ -0,0 +1,9 @@
+  window.onload = function () {
+    new Typed('.mov', {
+      strings:["Welcome!","We shortlist colleges for you!!", "Don't Get Puzzeled on finding colleges that suites your need.", "This is prototype, We will improve us."],
+      typeSpeed: 20,
+      backSpeed: 5,
+      showCursor: false,
+      loop: true
+    });
+    }
